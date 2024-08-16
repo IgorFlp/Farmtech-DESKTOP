f632929e1a67f856847e00a3c811767ca9a047ff
IgorFlp <igor.felipe.13@hotmail.com>
2024-08-16 00:16:01 -0300

    Reorganização do projeto

---
+++ Farmtech.DAO/UsuarioDAO.cs
@@ -0,0 +1,40 @@
+﻿using Farmtech.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Farmtech.DAO
+{
+    public class UsuarioDAO
+    {
+        //Cad usuario CRIA, DELETA, ALTERA, OU VISUALIZA Tb_Usuario
+        public void CriaUsuario(UsuarioEnt usuario) { }
+        //{
+        //    string queryString = "INSERT INTO dbo.Usuario(nome, usuario, senha) VALUES(@nome, @usuario, @senha))";
+        //    using(SqlConnection connection = new SqlConnection(connectionString)){                
+        //        connection.Open();
+        //        using (SqlCommand command = new SqlCommand(queryString, connection))
+        //            command.Parameters.AddWithValue('@nome', objeto.Nome);
+        //            command.Parameters.AddWithValue('@nome', objeto.usuario);
+        //            command.Parameters.AddWithValue('@nome', objeto.senha);                
+        //            command.ExecuteNonQuery();
+        //    }
+        //}
+        //Login VISUALIZA existencia do usuario e retorna erro ou efetua login
+        public static string LoginDAO(UsuarioEnt usuario)
+        {
+            if (usuario.Nome == "Jorge" && usuario.Senha == "123")
+            {
+                return "Correto";
+            }
+            else
+            {
+                return "Incorreto";
+            }
+        }
+    }
+}
---
+++ Farmtech.Model/LoginModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using Farmtech.Controller;
+using Farmtech.Entidades;
+
+
+
+namespace Farmtech.Model
+{
+
+    public class LoginModel
+    {
+        public static event Action<string> resultLogin;
+        public static void logar(string nome, string senha)
+        {
+            UsuarioEnt usuario = new UsuarioEnt();
+            LoginCtr loginCtr = new LoginCtr();
+            usuario.Nome = nome;
+            usuario.Senha = senha;
+            string res = loginCtr.Logar(usuario);
+            resultLogin?.Invoke(res);
+        }
+    }
+}
+            
+        
+    
+
